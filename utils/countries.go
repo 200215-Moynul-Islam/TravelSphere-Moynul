6e8753e9e59dad7ccd3bd17806d2d50e4b2b0c53
Moynul Islam <moynul200215@gmail.com>
2026-06-09 13:34:22 +0600

    feat: implement an util method to retrieve countries by region

--- utils/countries.go
+++ utils/countries.go
@@ -83,3 +83,28 @@ func GetAllCountries() ([]CountryDTO, error) {
 
 	return countries, nil
 }
+
+func GetCountriesByRegion(region string) ([]CountryDTO, error) {
+    baseURL, err := beego.AppConfig.String("restcountries_base_url")
+    if err != nil || baseURL == "" {
+        baseURL = "https://restcountries.com/v3.1"
+    }
+
+    url := fmt.Sprintf("%s/region/%s?fields=%s", baseURL, region, constants.RestCountriesFields)
+    apiResponse, err := http.Get(url)
+    if err != nil {
+        return nil, fmt.Errorf("failed connection: %w", err)
+    }
+    defer apiResponse.Body.Close()
+
+    if apiResponse.StatusCode != http.StatusOK {
+        return nil, fmt.Errorf("api error status: %d", apiResponse.StatusCode)
+    }
+
+    var countries []CountryDTO
+    if err := json.NewDecoder(apiResponse.Body).Decode(&countries); err != nil {
+        return nil, fmt.Errorf("failed to decode json stream: %w", err)
+    }
+
+    return countries, nil
+}
